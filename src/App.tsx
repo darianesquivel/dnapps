2810d03b1eff7cfb973387e4443f13e3a58a5054
Darian Esquivel <darianesquivelf@gmail.com>
2025-07-24 16:03:28 -0300

    Force capitalization fix for Yahtzee folder to ensure compatibility with case-sensitive systems

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Yahtzee from "./pages/Yahtzee";
-
+import Home from "./pages/Home/index";
+import Yahtzee from "./pages/Yahtzee/index";
 
 function App() {
   return (
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/yahtzee" element={<Yahtzee />} />
+          <Route path="/" element={<Home/>} />
+          <Route path="/yahtzee" element={<Yahtzee/>} />
         </Routes>
       </Router>
   );
